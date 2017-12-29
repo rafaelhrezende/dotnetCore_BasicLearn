005d1bbd6fcd1a3275532c4661f4acbe670bf561
rafael h rezende <rafaelhrezende@live.com>
2017-12-28 23:10:40 -0200

    New Project added: Web Api - Authentication with Identity and Jason Web token

---
+++ WebApi.IdentityJwt/Entities/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiJwt.Entities
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=LocalHost;Username=postgres;Password=******;Database=ConsoleApp");
+        }
+
+    }
+}
